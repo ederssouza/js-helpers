0c791c7a3ca377d9028733629ceeea93b8cb2176
Eder Sampaio <edersampaio@outlook.com.br>
2021-07-01 17:17:19 -0300

    Import all modules

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export { sum, JStest } from './lib'
+import {
+  isArray,
+  isObject,
+  isBoolean,
+  isNumber,
+  isString,
+  isFunction,
+  isNull,
+  isValidArray,
+  isValidObject
+} from './checkTypes'
+import { createHideLink } from './dom'
+
+export {
+  isArray,
+  isObject,
+  isBoolean,
+  isNumber,
+  isString,
+  isFunction,
+  isNull,
+  isValidArray,
+  isValidObject,
+  createHideLink
+}
