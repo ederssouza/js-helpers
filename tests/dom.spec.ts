94063f2e30909e024593572492c1a18c1c5a73a6
Eder Sampaio <edersampaio@outlook.com.br>
2021-07-01 17:16:38 -0300

    Add dom module test

---
+++ tests/dom.spec.ts
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+import 'jsdom-global/register'
+// import { expect } from 'chai'
+// import { createHideLink } from '../src/dom'
+
+describe('function createHideLink () {}', () => {
+  it('', () => {
+  })
+})
